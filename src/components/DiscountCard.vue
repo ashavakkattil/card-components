7a9fef4a041e636f03fec9e094cfbc184efcc8ba
Asha <asha.vakkattil@gmail.com>
2020-05-06 18:40:35 +0530

    Discount-card with slanting new banner

---
+++ src/components/DiscountCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row justify-center">
+    <q-card class="my-card" flat bordered>
+      <q-img
+        src="https://cdn.shopify.com/s/files/1/0815/1651/products/florida_eco_safari_1_large.jpeg?v=1427723864"
+      >
+        <div class="product-badge"><span class="text-uppercase">new</span></div>
+      </q-img>
+      <q-card-section class="text-center">
+        <div class="text-h6">Florida Eco Safari</div>
+        <div
+          class="text-subtitle1 card-text"
+        >This is Florida eco safari.Of course you can rely on your GPS system</div>
+        <div class="q-mt-md">
+          <span class="text-h6 text-teal q-mr-md">$410.00</span>
+          <span class="text-h6 text-teal-3 q-mr-md old-price">$500.00</span>
+        </div>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+<style lang="scss" scoped>
+.my-card {
+  width: 300px;
+  .card-text {
+    line-height: 1.2em;
+    color: grey;
+  }
+}
+.old-price {
+  text-decoration: line-through;
+}
+.product-badge {
+    padding: 5px;
+    width: 156px;
+    text-align: center;
+    transform: rotate(-45deg);
+    position: relative;
+    left: -45px;
+    top: 30px;
+    background-color: #11bf8a
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,13 @@
 <template>
-  <q-page class="q-ma-xl"></q-page>
+  <q-page class="q-ma-xl">
+    <discount-card></discount-card>
+  </q-page>
 </template>
 
 <script>
+import DiscountCard from "../components/DiscountCard";
 export default {
-  name: "PageIndex"
+  name: "PageIndex",
+  components: { DiscountCard }
 };
 </script>
